7e5136ffd44be8c3e5acdb7b7cfe843473b7d70b
umayanga-prabhash <umayangag@gmail.com>
2019-03-02 16:40:20 +0530

    reformat code

--- public/js/update-schedule.js
+++ public/js/update-schedule.js
@@ -1,22 +1,22 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    const switching_time=1000; //time in seconds
-    let api_url="http://localhost:8080/api/screen/schedule";
+    const switching_time = 1000; //time in seconds
+    let api_url = "http://localhost:8080/api/screen/schedule";
 
-    let last_fetched=[];
+    let last_fetched = [];
 
-    function get_schedule_from_server(api_url){
+    function get_schedule_from_server(api_url) {
         $.ajax({
             type: "GET",
             url: api_url,
             dataType: 'json', // json method
             success: function (response) { // If success
-                last_fetched=response;
+                last_fetched = response;
             },
             error: function (response) { // If success
                 console.log('error loading at ', new Date($.now()));
             }
-    });
+        });
     }
 
     function update_date(currentDate) {
@@ -27,20 +27,22 @@ $( document ).ready(function() {
         $('.header-title').html(headerTitle);
     }
 
-    function update_table(screenRows){
+    function update_table(screenRows) {
         clear_data_fields();
 
-        for (let i=0; i<screenRows.length;i++){
-            screenRow=screenRows[i];
+        for (let i = 0; i < screenRows.length; i++) {
+            screenRow = screenRows[i];
 
-            $('#row-'+(i+1)).removeClass();
-            $('#row-'+(i+1)).addClass(screenRow['status']);
+            let row = $('#row-' + (i + 1));
 
-            $('.time-'+(i+1)).html(screenRow['time']);
-            $('.destination-'+(i+1)).html(screenRow['destination']);
-            $('.route-'+(i+1)).html(screenRow['route']);
-            $('.remarks-'+(i+1)).html(screenRow['remarks']);
-            $('.status-'+(i+1)).html(screenRow['status']);
+            row.removeClass();
+            row.addClass(screenRow['status']);
+
+            $('.time-' + (i + 1)).html(screenRow['time']);
+            $('.destination-' + (i + 1)).html(screenRow['destination']);
+            $('.route-' + (i + 1)).html(screenRow['route']);
+            $('.remarks-' + (i + 1)).html(screenRow['remarks']);
+            $('.status-' + (i + 1)).html(screenRow['status']);
         }
 
     }
@@ -54,9 +56,17 @@ $( document ).ready(function() {
     update_header_title(last_fetched['screenTitle']);
     update_table(last_fetched);
 
-    self.setInterval(function(){get_schedule_from_server(api_url)},switching_time);
-    self.setInterval(function(){update_date(last_fetched['currentDate'])},switching_time);
-    self.setInterval(function(){update_header_title(last_fetched['screenTitle'])},switching_time);
-    self.setInterval(function(){update_table(last_fetched['screenRows'])},switching_time);
+    self.setInterval(function () {
+        get_schedule_from_server(api_url)
+    }, switching_time);
+    self.setInterval(function () {
+        update_date(last_fetched['currentDate'])
+    }, switching_time);
+    self.setInterval(function () {
+        update_header_title(last_fetched['screenTitle'])
+    }, switching_time);
+    self.setInterval(function () {
+        update_table(last_fetched['screenRows'])
+    }, switching_time);
 
 });
