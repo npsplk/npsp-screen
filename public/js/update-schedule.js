0ea8f3de67e4d0c9452ee0ef372058901f70ed40
umayanga-prabhash <umayangag@gmail.com>
2019-03-03 10:13:01 +0530

    clear initial screen and reformat

--- public/js/update-schedule.js
+++ public/js/update-schedule.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    const switching_time = 1000; //time in seconds
+    const switching_time = 3000; //time in seconds
     let api_url = "http://localhost:8080/api/screen/schedule";
 
     let last_fetched = [];
@@ -48,30 +48,26 @@ $(document).ready(function () {
             switch_language('#header-route', [table_headers[0][2], table_headers[1][2], table_headers[2][2]]);
             switch_language('#header-status', [table_headers[0][3], table_headers[1][3], table_headers[2][3]]);
         }
-    // <td id="header-time">TIME</td>
-    //         <td id="header-destination">DESTINATION</td>
-    //         <td id="header-route">ROUTE</td>
-    //         <td id="header-status">STATUS</td>
-    //         <td id="header-remarks">REMARKS</td>
     }
 
     function update_table(screen_rows) {
-        clear_data_fields();
+        if(typeof screen_rows !== 'undefined') {
+            clear_data_fields();
 
-        for (let i = 0; i < screen_rows.length; i++) {
-            let screen_row = screen_rows[i];
-            let table_row = $('#row-' + (i + 1));
+            for (let i = 0; i < screen_rows.length; i++) {
+                let screen_row = screen_rows[i];
+                let table_row = $('#row-' + (i + 1));
 
-            table_row.removeClass();
-            table_row.addClass(screen_row['status']);
+                table_row.removeClass();
+                table_row.addClass(screen_row['status']);
 
-            $('.time-' + (i + 1)).html(screen_row['time']);
-            $('.destination-' + (i + 1)).html(screen_row['destination']);
-            $('.route-' + (i + 1)).html(screen_row['route']);
-            $('.remarks-' + (i + 1)).html(screen_row['remarks']);
-            $('.status-' + (i + 1)).html(screen_row['status']);
+                $('.time-' + (i + 1)).html(screen_row['time']);
+                $('.destination-' + (i + 1)).html(screen_row['destination']);
+                $('.route-' + (i + 1)).html(screen_row['route']);
+                $('.remarks-' + (i + 1)).html(screen_row['remarks']);
+                $('.status-' + (i + 1)).html(screen_row['status']);
+            }
         }
-
     }
 
     function clear_data_fields() {
@@ -83,7 +79,7 @@ $(document).ready(function () {
         update_date(last_fetched['currentDate']);
         update_title(last_fetched['screenTitle']);
         update_table_headers(last_fetched['tableHeaders']);
-        // update_table(last_fetched['screenRows']);
+        update_table(last_fetched['screenRows']);
     }
 
     refresh_content();
