42c0825fce68272df7beaedc00172fe1d6f80cd6
umayanga-prabhash <umayangag@gmail.com>
2019-03-03 16:40:06 +0530

    schedule phase 1

--- public/js/update-schedule.js
+++ public/js/update-schedule.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-    const switching_time = 3000; //time in seconds
+    let switching_time = 5000; //time in seconds
     let api_url = "http://localhost:8080/api/screen/schedule";
 
     let last_fetched = [];
 
-    function switch_language(identifier,lang_list){
-        if(typeof lang_list !== 'undefined') {
+    function switch_language(identifier, lang_list) {
+        if (typeof lang_list !== 'undefined') {
             $(identifier).html(lang_list[0]);
 
             setTimeout(function () {
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $(identifier).html(lang_list[2]);
-            }, switching_time*2);
+            }, switching_time * 2);
         }
     }
 
@@ -38,11 +38,11 @@ $(document).ready(function () {
     }
 
     function update_title(title) {
-        switch_language('.header-title',title);
+        switch_language('.header-title', title);
     }
 
-    function update_table_headers(table_headers){
-        if(typeof table_headers !== 'undefined') {
+    function update_table_headers(table_headers) {
+        if (typeof table_headers !== 'undefined') {
             switch_language('#header-time', [table_headers[0][0], table_headers[1][0], table_headers[2][0]]);
             switch_language('#header-destination', [table_headers[0][1], table_headers[1][1], table_headers[2][1]]);
             switch_language('#header-route', [table_headers[0][2], table_headers[1][2], table_headers[2][2]]);
@@ -51,7 +51,7 @@ $(document).ready(function () {
     }
 
     function update_table(screen_rows) {
-        if(typeof screen_rows !== 'undefined') {
+        if (typeof screen_rows !== 'undefined') {
             clear_data_fields();
 
             for (let i = 0; i < screen_rows.length; i++) {
@@ -62,10 +62,10 @@ $(document).ready(function () {
                 table_row.addClass(screen_row['status']);
 
                 $('.time-' + (i + 1)).html(screen_row['time']);
-                $('.destination-' + (i + 1)).html(screen_row['destination']);
                 $('.route-' + (i + 1)).html(screen_row['route']);
                 $('.remarks-' + (i + 1)).html(screen_row['remarks']);
-                switch_language('.status-' + (i + 1),screen_row['status'])
+                switch_language('.destination-' + (i + 1), screen_row['destination']);
+                switch_language('.status-' + (i + 1), screen_row['status']);
             }
         }
     }
@@ -74,7 +74,7 @@ $(document).ready(function () {
         $('.data-field').html("");
     }
 
-    function refresh_content(){
+    function refresh_content() {
         get_schedule_from_server(api_url);
         update_date(last_fetched['currentDate']);
         update_title(last_fetched['screenTitle']);
@@ -86,6 +86,6 @@ $(document).ready(function () {
 
     self.setInterval(function () {
         refresh_content()
-    }, switching_time*3);
+    }, switching_time * 3);
 
 });
